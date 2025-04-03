88167a5ccffd62d4beb80527daf520255dd5e2db
ChhayMonyneath123 <neath5738@gmail.com>
2025-04-03 17:07:40 +0700

    commit

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,7 +1,12 @@
-import './bootstrap';
+import './bootstrap';                
+import { createApp } from 'vue';    
+import Alpine from 'alpinejs';    
+import MainApp from './components/mainApp.vue'; 
+import router from './router'
 
-import Alpine from 'alpinejs';
+window.Alpine = Alpine;       
+Alpine.start();                     
 
-window.Alpine = Alpine;
-
-Alpine.start();
+const app = createApp(MainApp);     
+app.use(router)
+app.mount('#app')               
