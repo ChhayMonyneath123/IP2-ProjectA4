adc046111cebdb0e36935931dc851cb0ec75a180
Chhay Monyneath <144520475+ChhayMonyneath123@users.noreply.github.com>
2025-05-31 15:10:41 +0700

    Update Drink.vue

--- IP2-frontend/src/views/User/categories/Drink.vue
+++ IP2-frontend/src/views/User/categories/Drink.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="drink">
         <Nav_bar />
-<<<<<<< HEAD
-=======
         <Breadcrumb/>
->>>>>>> Neath
         <div class="drink-content">
             <div class="drink-nav-link">
                 <span><router-link to="/" class="disable">Home &gt;</router-link></span>
@@ -18,35 +15,22 @@
         <div class="drink-menu">
             <h2>picture of Drink</h2>
         </div>
-<<<<<<< HEAD
-        <FooterBar />
-    </div>
-=======
-        
     </div>
     <Footer_bar/>
->>>>>>> Neath
 </template>
 
 <script>
 import Nav_bar from '@/components/nav_bar.vue';
-<<<<<<< HEAD
-import FooterBar from '@/components/FooterBar.vue';
-
-=======
 import Footer_bar from '@/components/footer_bar.vue';
 import Breadcrumb from '@/components/breadcrumb.vue';
->>>>>>> Neath
+
 export default {
 name: "drink",
 components: {
     Nav_bar,
-<<<<<<< HEAD
-    FooterBar
-=======
     Footer_bar,
     Breadcrumb
->>>>>>> Neath
+
 }
 }
 </script>
@@ -56,11 +40,7 @@ components: {
 
 flex-direction: column;
 width: 100%;
-<<<<<<< HEAD
-min-height: 100vh;
-=======
 height: 100vh;
->>>>>>> Neath
 background-color: #f9f9f9;
 }
 
