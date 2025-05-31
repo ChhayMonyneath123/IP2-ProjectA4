a2bac3799193880b4dc0a573d54df5b3e8e106bb
ChhayMonyneath123 <neath5738@gmail.com>
2025-05-31 22:20:57 +0700

    update change

--- IP2-frontend/src/views/User/authentication/My_order.vue
+++ IP2-frontend/src/views/User/authentication/My_order.vue
@@ -4,29 +4,16 @@
         <div class="">
             <h1>welcome to my-order</h1>
         </div>
-<<<<<<< HEAD
-        <FooterBar/>
-=======
         <Footer_bar/>
->>>>>>> Neath
     </div>
 </template>
 <script>
 import Nav_bar from '@/components/nav_bar.vue';
-<<<<<<< HEAD
-import FooterBar from '@/components/FooterBar.vue';
-
-    export default {
-        name: "my-order",
-        components:{
-            FooterBar,
-=======
 import Footer_bar from '@/components/footer_bar.vue';
     export default {
         name: "my-order",
         components:{
             Footer_bar,
->>>>>>> Neath
             Nav_bar
         }
     }
