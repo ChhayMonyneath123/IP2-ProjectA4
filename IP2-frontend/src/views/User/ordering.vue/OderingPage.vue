a9e1f70d31512979a171e07d4d28c074a38bad8c
Chhay Monyneath <144520475+ChhayMonyneath123@users.noreply.github.com>
2025-05-31 15:12:31 +0700

    Update OderingPage.vue

--- IP2-frontend/src/views/User/ordering.vue/OderingPage.vue
+++ IP2-frontend/src/views/User/ordering.vue/OderingPage.vue
@@ -1,25 +1,16 @@
 <template>
     <div class="orderingPage">
         <Nav_bar/>
-<<<<<<< HEAD
-        <div class="ordering">
-            <h1>welcome to ordering</h1>
-        </div>
-        <FooterBar/>
-=======
         <Breadcrumb/>
         <div class="ordering">
             <h1>welcome to ordering</h1>
         </div>
         <Footer_bar/>
->>>>>>> Neath
+
     </div>
 </template>
 <script>
 import Nav_bar from '@/components/nav_bar.vue';
-<<<<<<< HEAD
-import FooterBar from '@/components/FooterBar.vue';
-=======
 import Footer_bar from '@/components/footer_bar.vue';
 import Breadcrumb from '@/components/breadcrumb.vue';
 >>>>>>> Neath
@@ -27,14 +18,9 @@ import Breadcrumb from '@/components/breadcrumb.vue';
     export default {
         name: "Drink",
         components:{
-<<<<<<< HEAD
-            FooterBar,
-            Nav_bar
-=======
             Footer_bar,
             Nav_bar,
             Breadcrumb
->>>>>>> Neath
         }
     }
 </script>
