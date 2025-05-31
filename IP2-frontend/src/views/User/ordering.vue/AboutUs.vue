a1cc95797a7995b997e7cd7dfbae7a2058d2c5c8
Chhay Monyneath <144520475+ChhayMonyneath123@users.noreply.github.com>
2025-05-31 15:11:26 +0700

    Update AboutUs.vue

--- IP2-frontend/src/views/User/ordering.vue/AboutUs.vue
+++ IP2-frontend/src/views/User/ordering.vue/AboutUs.vue
@@ -1,41 +1,26 @@
 <template>
   <div class="aboutContain">
     <Nav_bar/>
-<<<<<<< HEAD
-      <div class="about">
-        <h1>welcome to about</h1>
-      </div>
-    <FooterBar/>
-=======
     <Breadcrumb/>
       <div class="about">
         <h1>welcome to about</h1>
       </div>
     <Footer_bar/>
->>>>>>> Neath
+
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import FooterBar from '@/components/FooterBar.vue';
-=======
 import Breadcrumb from '@/components/breadcrumb.vue';
 import Footer_bar from '@/components/footer_bar.vue';
->>>>>>> Neath
 import Nav_bar from '@/components/nav_bar.vue';
 
 export default {
   name: "AboutUs",
   components:{
-<<<<<<< HEAD
-    FooterBar,
-    Nav_bar
-=======
     Footer_bar,
     Nav_bar,
     Breadcrumb
->>>>>>> Neath
   }
 }
 </script>
