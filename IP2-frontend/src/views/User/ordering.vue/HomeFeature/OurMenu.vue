cc7504eed03fe56d97a96f1a20a721d96dee2999
ChhayMonyneath123 <neath5738@gmail.com>
2025-06-10 00:26:29 +0700

    Commit  table backend

--- IP2-frontend/src/views/User/ordering.vue/HomeFeature/OurMenu.vue
+++ IP2-frontend/src/views/User/ordering.vue/HomeFeature/OurMenu.vue
@@ -16,7 +16,7 @@
 
 <div class="menu-labels">
   <router-link to="/menu/food"><h2>Food</h2></router-link>
-  <router-link to="/menu/drinks"><h2>Drinks</h2></router-link>
+  <router-link to="/menu/drink"><h2>Drinks</h2></router-link>
   <router-link to="/menu/dessert"><h2>Dessert</h2></router-link>
 </div>
 
