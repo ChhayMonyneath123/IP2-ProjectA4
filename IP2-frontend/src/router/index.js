18358514d34907ae5ef41dd787b9b29c824ba636
HENGKalyan <kalyanheng99@gmail.com>
2025-06-22 15:18:43 +0700

    dashboard and delivery

--- IP2-frontend/src/router/index.js
+++ IP2-frontend/src/router/index.js
@@ -108,7 +108,7 @@ const routes = [
             {
                 path: 'delivery',
                 name: 'Delivery',
-                component: () => import('@/views/Admin/authentication/Delivery.vue')
+                component: () => import('@/views/Admin/authentication/DeliveryPage.vue')
             },
             {
                 path: 'chat',
