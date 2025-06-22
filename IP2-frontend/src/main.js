b222232e3de084206d48b300b56b82f91545269d
HENGKalyan <kalyanheng99@gmail.com>
2025-06-22 20:09:52 +0700

    update add to card

--- IP2-frontend/src/main.js
+++ IP2-frontend/src/main.js
@@ -3,15 +3,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import axios from 'axios'
-
 import App from './App.vue'
 import router from './router'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@fontsource/instrument-sans';
 import '@fontsource/ibm-plex-sans-condensed';
-const app = createApp(App)
 
-app.use(createPinia())
+// Set axios baseURL BEFORE creating the app
+axios.defaults.baseURL = 'http://localhost:8000'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)  // You had createPinia() twice - fixed this too
 app.use(router)
 
 app.mount('#app')
